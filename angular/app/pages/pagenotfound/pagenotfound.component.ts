124bd45b3efab876432472b0e4fd17644d0b4d4a
andrecarnide <andre_fac@hotmail.com>
2017-01-30 23:13:04 +0000

    Delete pagenotfound.component.ts

--- angular/app/pages/pagenotfound/pagenotfound.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'app-home',
-  templateUrl: './pagenotfound.component.html'
-})
-
-export class PageNotFoundComponent { }
