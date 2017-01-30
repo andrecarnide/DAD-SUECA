28dacd06fb67b257056424f55ceed5feec2c7023
andrecarnide <andre_fac@hotmail.com>
2017-01-30 23:14:16 +0000

    Add files via upload

---
+++ angular/app/pages/register/register.component.js
@@ -0,0 +1,58 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
+var authentication_service_1 = require('../../services/authentication.service');
+var websocket_service_1 = require('../../services/websocket.service');
+var user_1 = require('../../models/user');
+var RegisterComponent = (function () {
+    function RegisterComponent(webSocketService, authenticationService, router) {
+        this.webSocketService = webSocketService;
+        this.authenticationService = authenticationService;
+        this.router = router;
+        this.user = new user_1.User('', '', '', '', 0, 0, '', '', '', '');
+    }
+    RegisterComponent.prototype.register = function () {
+        var _this = this;
+        this.isRegisterFailed = false;
+        this.authenticationService.register(this.user.name, this.user.username, this.user.email, this.user.password, this.user.avatar).subscribe(function (registered) {
+            if (registered) {
+                if (registered['message'] === 'This user already exists') {
+                    _this.usernameEqual = true;
+                }
+                else if (registered['message'] === 'This Email already exists') {
+                    _this.emailEqual = true;
+                }
+                else {
+                    _this.isRegisterFailed = false;
+                    //this.webSocketService.sendRegisterMessage(registered);
+                    _this.router.navigateByUrl('/login');
+                    alert('User created successfully!');
+                }
+            }
+            else {
+                _this.isRegisterFailed = true;
+            }
+        });
+    };
+    RegisterComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'app-home',
+            templateUrl: './register.component.html',
+            styleUrls: ['./register.component.css']
+        }), 
+        __metadata('design:paramtypes', [websocket_service_1.WebSocketService, authentication_service_1.AuthenticationService, router_1.Router])
+    ], RegisterComponent);
+    return RegisterComponent;
+}());
+exports.RegisterComponent = RegisterComponent;
+//# sourceMappingURL=register.component.js.map
---
+++ angular/app/pages/register/register.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../../services/authentication.service';
+import { WebSocketService } from '../../services/websocket.service';
+
+import { User }    from '../../models/user';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-home',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
+})
+
+export class RegisterComponent {
+    isRegisterFailed: boolean;
+    usernameEqual: boolean;
+    emailEqual: boolean;
+    user = new User('', '', '', '', 0, 0, '', '', '', '');
+
+    constructor(private webSocketService: WebSocketService, private authenticationService: AuthenticationService, private router: Router) {
+    }
+
+    register() {
+        this.isRegisterFailed = false;
+
+        this.authenticationService.register(this.user.name, this.user.username, this.user.email, this.user.password, this.user.avatar).subscribe(registered => {
+            if (registered) {
+                if (registered['message'] === 'This user already exists') {
+                    this.usernameEqual = true;
+                } else if (registered['message'] === 'This Email already exists') {
+                    this.emailEqual = true;
+                } else {
+                    this.isRegisterFailed = false;
+                    //this.webSocketService.sendRegisterMessage(registered);
+                    this.router.navigateByUrl('/login');
+                    alert('User created successfully!');
+                }
+            } else {
+                this.isRegisterFailed = true;
+            }
+        });
+    }
+}
+
