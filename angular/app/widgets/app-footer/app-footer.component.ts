07e98c1646b4dad57e9c2f24815eea50dd797d3d
andrecarnide <andre_fac@hotmail.com>
2017-01-30 23:09:54 +0000

    Delete app-footer.component.ts

--- angular/app/widgets/app-footer/app-footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'app-footer',
-    templateUrl: './app-footer.component.html'
-})
-export class AppFooterComponent { }
