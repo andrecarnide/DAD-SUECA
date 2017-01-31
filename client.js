2911f6fa7b7934b1b7d54c94ce8254e4ad91f648
andrecarnide <andre_fac@hotmail.com>
2017-01-31 17:20:09 +0000

    Delete client.js

--- client.js
+++
@@ -1,4 +0,0 @@
-var socket = require('socket.io-client')('http://localhost:8080');
-socket.on('connect', () => console.log('connected'));
-socket.on('players', (m) => console.log('>> %s', m));
-socket.on('disconnect', () => console.log('connected'));
