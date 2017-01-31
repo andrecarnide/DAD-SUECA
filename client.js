8c0c1c323ae8adf772d273dacaa666baf1f16038
andrecarnide <andre_fac@hotmail.com>
2017-01-31 16:37:27 +0000

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var socket = require('socket.io-client')('http://localhost:7777');
+var socket = require('socket.io-client')('http://localhost:8080');
 socket.on('connect', () => console.log('connected'));
 socket.on('players', (m) => console.log('>> %s', m));
 socket.on('disconnect', () => console.log('connected'));
