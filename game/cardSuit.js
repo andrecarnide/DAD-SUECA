75cdcd66e2d3a71adc1df1323695f280aa065567
andrecarnide <andre_fac@hotmail.com>
2017-01-31 17:24:14 +0000

    Delete cardSuit.js

--- game/cardSuit.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var cardSuit;
-(function (cardSuit) {
-    cardSuit[cardSuit["SPADE"] = 0] = "SPADE";
-    cardSuit[cardSuit["HEART"] = 1] = "HEART";
-    cardSuit[cardSuit["CLUB"] = 2] = "CLUB";
-    cardSuit[cardSuit["DIAMOND"] = 3] = "DIAMOND";
-})(cardSuit = exports.cardSuit || (exports.cardSuit = {}));
